f99f11b43e9e93e22ac2509cb436a9ddfeaf38ec
anthonykimani <kimaniantoni@gmail.com>
2023-01-25 15:09:44 +0300

    initilizes repo with react boilerplate, tailwind installation

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./components/Home";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className="App">
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+        </Routes>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
